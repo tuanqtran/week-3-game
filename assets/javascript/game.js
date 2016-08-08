c075c1cbc612a53abc42bc685b07a15e093ea9d6
Tuan Tran <tuan.tran1122@gmail.com>
2016-08-08 15:04:53 -0500

    Added comments to appropriate areas

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 // Create the a-z list for tihe computer choices
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
         "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
 // var computerChoices = "abcdefghijklmnopqrstuvwxyz".split("");
+
 // Wins/Losses tally declaration set to 0
 var wins = 0;
 var losses = 0;
@@ -14,6 +14,17 @@ var guessesSoFar = [];
 document.onkeyup = function(event){
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    //Making sure the user chooses a-z
+    if ((userGuess == 'a') || (userGuess == 'b') || (userGuess == 'c') ||
+        (userGuess == 'd') || (userGuess == 'e') || (userGuess == 'f') ||
+        (userGuess == 'g') || (userGuess == 'h') || (userGuess == 'i') ||
+        (userGuess == 'j') || (userGuess == 'k') || (userGuess == 'l') ||
+        (userGuess == 'm') || (userGuess == 'n') || (userGuess == 'o') ||
+        (userGuess == 'p') || (userGuess == 'q') || (userGuess == 'r') ||
+        (userGuess == 's') || (userGuess == 't') || (userGuess == 'u') ||
+        (userGuess == 'v') || (userGuess == 'w') || (userGuess == 'x') ||
+        (userGuess == 'y') || (userGuess == 'z')){
 
         if (userGuess == computerGuess){
                 
@@ -41,37 +52,19 @@ document.onkeyup = function(event){
             }
 
             var html = "<h1>The Psychic Game</h1>" +
+
             "<p>Guess what letter I'm thinking of</p>" +
+            // # of times the user has guessed the letter correctly
             "<p>Wins: " + wins + "</p>" +
+            // # of times the user has failed to guess the letter correctly after exhausting all guesses
             "<p>Losses: " + losses + "</p>" +
+            // # of guesses left.
             "<p>Guesses left: " + guesses + "</p>" +
+            // The specific letters that the user typed. Display these until the user either wins or loses.
             "<p>Your Guesses so far: " +  guessesSoFar + "</p>";
 
             document.querySelector(".game").innerHTML = html;
+        }else{
+            alert("Invalid letter. Please type a letter from a-z.");
         }
-
-//Making sure the user chooses a-z
-
-    
-
-// Wins: (# of times the user has guessed the letter correctly)
-
-
-// Losses: (# of times the user has failed to guess the letter 
-// correctly after exhausting all guesses)
-
-
-// Guesses Left: (# of guesses left. This will update)
-
-
-// Your Guesses So Far: (the specific letters that the user typed.
-// Display these until the user either wins or loses.)
-
-
-// When the player wins, increase the Wins counter and start
-// the game over again (without refreshing the page).
-
-
-// When the player loses, increase the Losses counter and restart
-// the game without a page refresh (just like when the user wins).
-
+    }
