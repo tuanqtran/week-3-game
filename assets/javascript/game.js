e43b47c13e99e54f30a018fbbb0c1931d0029791
Tuan Tran <tuan.tran1122@gmail.com>
2016-08-12 15:09:45 -0500

    Troubleshooting duplicate lettering

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,31 @@
-// Create the a-z list for tihe computer choices
+// Create the a-z list for tihe computer choices.
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
         "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// var computerChoices = "abcdefghijklmnopqrstuvwxyz".split("");
-// var computerChoices = ["a", "b"];
 
-// Wins/Losses tally declaration set to 0
+// Wins/Losses tally declaration set to 0.
 var wins = 0;
 var losses = 0;
-// Your guesses left so far set to 9 (Will decrement.)
+// Your guesses left so far set to 9 (Will decrement.).
 var guesses = 9;
 var guessesSoFar = [];
 
+// Guess what letter I'm thinking of.
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 console.log("First computerGuess: " + computerGuess);
 
+// Function reset upon wins/losses.
 function computerGuessReset(){
     computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
     guessesSoFar = []
     guesses = 9
 }
 
-// Guess what letter I'm thinking of
+// Takes user key press and turns it into variable userGuess.
 document.onkeyup = function(event){
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
 
-    //Making sure the user chooses a-z
+    // Makes sure the user chooses a letter between a-z.
     if ((userGuess == 'a') || (userGuess == 'b') || (userGuess == 'c') ||
         (userGuess == 'd') || (userGuess == 'e') || (userGuess == 'f') ||
         (userGuess == 'g') || (userGuess == 'h') || (userGuess == 'i') ||
@@ -36,6 +36,7 @@ document.onkeyup = function(event){
         (userGuess == 'v') || (userGuess == 'w') || (userGuess == 'x') ||
         (userGuess == 'y') || (userGuess == 'z')){
 
+        // Wins if userGuess = to computerGuess.
         if (userGuess == computerGuess){
             wins++
             computerGuessReset();
@@ -43,6 +44,8 @@ document.onkeyup = function(event){
             console.log("-------------------------");
             console.log("ComputerGuess new letter after win: " + computerGuess);
             console.log("-------------------------");
+
+        // Losses when the guess counter hits 0.
         }else if (guesses == 1){
             losses++
             computerGuessReset();
@@ -50,35 +53,42 @@ document.onkeyup = function(event){
             console.log("-------------------------");
             console.log("ComputerGuess new letter after loss: " + computerGuess);
             console.log("-------------------------");
+        // Decrements guesses when userGuess != computerGuess.
         }else{
-            // for(var n = 0; n < guessesSoFar.length; n++){
-            //     if(userGuess.indexOf(guessesSoFar[n]) > -1){
+            // Create a loop that will compare the entire guessesSoFar array with the userGuess.
+            // If userGuess is inside the guessesSoFar array, alert duplicate letter.
+            for(var n = 0; n < guessesSoFar.length; n++){
+                if(userGuess.indexOf(guessesSoFar[n]) > -1){
                     guesses--
                     guessesSoFar.push(userGuess);
                     console.log(userGuess);
-            //     }else{
-            //         alert("Duplicate letter was pressed. Please try again.")
-            //     }
-            // }
-        }     
+                }else{
+                    alert("Duplicate letter was pressed. Please try again.")
+                }
+            }
+        }
+    // Alerts user if a invalid letter is pressed.
     }else{
         alert("Invalid letter. Please type a letter from a-z.");
         console.log("Alerts false letter: " + userGuess);
         console.log("-------------------------");
     }
 
-    var html = "<h1>The Psychic Game</h1>" +
-    "<p>Guess what letter I'm thinking of</p>" +
-    // # of times the user has guessed the letter correctly
-    "<p>Wins: " + wins + "</p>" +
-    // # of times the user has failed to guess the letter correctly after exhausting all guesses
-    "<p>Losses: " + losses + "</p>" +
-    // # of guesses left.
-    "<p>Guesses left: " + guesses + "</p>" +
-    // The specific letters that the user typed. Display these until the user either wins or loses.
-    "<p>Your Guesses so far: " +  guessesSoFar + "</p>";
+// Prints the html layout below upon userGuess input.
+var html = "<h1>The Psychic Game</h1>" +
+"<p>Guess what letter I'm thinking of</p>" +
+// # of times the user has guessed the letter correctly
+"<p>Wins: " + wins + "</p>" +
+// # of times the user has failed to guess the letter correctly after exhausting all guesses
+"<p>Losses: " + losses + "</p>" +
+// # of guesses left.
+"<p>Guesses left: " + guesses + "</p>" +
+// The specific letters that the user typed. Display these until the user either wins or loses.
+"<p>Your Guesses so far: " +  guessesSoFar + "</p>";
 
-    document.querySelector(".game").innerHTML = html;
+document.querySelector(".game").innerHTML = html;
 }
 
+// var computerChoices = "abcdefghijklmnopqrstuvwxyz".split("");
+// var computerChoices = ["a", "b"];
 
